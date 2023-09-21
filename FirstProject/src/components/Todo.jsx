5cde837745175a48fb9b249e831c080e1a090f91
gonzaloavila01 <gonzaloavila2001avila@gmail.com>
2023-09-20 20:01:50 -0600

    Unidad 2 - Tarea 3

--- FirstProject/src/components/Todo.jsx
+++ FirstProject/src/components/Todo.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet, Text, Modal } from "react-native";
 import CustomButton from "./CustomButton";
+import { ModalTodo } from "./ModalTodo";
 
 const Todo = ({ id, name, handleDelete, handleComplete, isCompleted, dateAdded, handleUpdate, updated, isUpdating }) => {
   
@@ -12,6 +14,13 @@ const Todo = ({ id, name, handleDelete, handleComplete, isCompleted, dateAdded, 
     }
   }
   
+  const [modalVisible, setModalVisible] = useState(false)
+
+  const muestraModal = () =>{
+    setModalVisible(true)
+  }
+
+
   return (
     <View style={[styles.container, isCompleted && styles.todoCompleted]}>
       <View>
@@ -24,7 +33,22 @@ const Todo = ({ id, name, handleDelete, handleComplete, isCompleted, dateAdded, 
           <CustomButton light={isCompleted ? false : true} iconName={'trash'} onPress={() => handleDelete(id)} />
           <CustomButton  light={isCompleted ? false : true} iconName={isUpdating? 'remove':'edit'} onPress={()=> handleUpdate(id)}/>
           <CustomButton iconName={isCompleted ? 'arrow-circle-o-left' : 'check-circle-o'} light={isCompleted ? false : true} onPress={() => handleComplete(id)} />
+          <CustomButton light iconName={'eye'} onPress={() => muestraModal()}/>
         </View>
+
+        <Modal
+          animationType="fade"
+          visible={modalVisible}
+          transparent={true}
+          onRequestClose={() => {
+              setModalVisible(!modalVisible);
+          }}
+          >
+          <View style={styles.Modal}>
+              <ModalTodo id={id} name={name} dateAdded={dateAdded} updated={updated} isCompleted={isCompleted}/>    
+              <CustomButton light  iconName={'eye-slash'} onPress={() => setModalVisible(!modalVisible)}/>                
+          </View> 
+        </Modal>
       </View> 
       </View>
   );
@@ -57,6 +81,17 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 8,
   },
+  Modal:{
+    backgroundColor: 'black',
+    width: 250,
+    justifyContent:'center',
+    alignItems: 'center',
+    paddingBottom: 10,
+    borderRadius: 20,
+    gap: 10,
+    marginTop: 250,
+    marginLeft: '20%',
+},
 });
 
 export default Todo;
